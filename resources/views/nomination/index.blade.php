40260bc5026851cda1bcfce3bd9ca8d709865129
abiruzzamanabir <abiruzzamanabir17@gmail.com>
2025-09-01 16:37:53 +0600

    fix text

--- resources/views/nomination/index.blade.php
+++ resources/views/nomination/index.blade.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nomination Form | {{ $theme->title }}</title>
+    <title>Survey Form | {{ $theme->title }}</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
